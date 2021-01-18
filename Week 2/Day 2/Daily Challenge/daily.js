5d966c85e0b78d7e2a1093498737e459366a5848
Itamar1m <itamarym@gmail.com>
2021-01-18 22:58:23 +0200

    Daily Challenge

--- Week 2/Day 2/Daily Challenge/daily.js
+++ Week 2/Day 2/Daily Challenge/daily.js
@@ -1,10 +1,10 @@
-let string1 = '  not bad '
-let find = 'The index of not is ' + string1.indexOf('not') + ' the index of bad is ' + string1.indexOf('bad')
-console.log(find)
-if (string1.indexOf('not') < string1.indexOf('bad')) {
-    let string1 = 'Good'
-    console.log(string1)
-
-} else {
-    console.log(string1)
-}
+let string1 ='The  food is not bad   '
+        let find  = 'The index of not is '  +string1.indexOf('not')   +' the index of bad is '+  string1.indexOf('bad')
+        console.log(find)
+        if(string1.indexOf('not')<string1.indexOf('bad')&&string1.indexOf('not')>0){
+         let string1='Good'
+            console.log(string1)
+        
+        }else{
+        console.log(string1)
+        }
