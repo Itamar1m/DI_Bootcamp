65b760d0d6ca247ced322ff3bcd5a5b1f7cdbc8c
Itamar1m <77247099+Itamar1m@users.noreply.github.com>
2021-01-25 20:02:07 +0200

    Delete Mandatory excercises directory

--- Mandatory excercises/exc3.js
+++
@@ -1,5 +0,0 @@
-document.body.firstElementChild.style.backgroundColor='lightblue'
-let listItems =document.getElementsByTagName('li')
-listItems[0].innerHTML=''
-listItems[1].style.border='solid black 1px'
-document.body.style.fontSize='30px'
