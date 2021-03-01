b37e2222947b6adbbd74f94edbf799dd7f62c70d
Itamar1m <itamarym@gmail.com>
2021-03-01 22:36:33 +0200

    EXC dj day 2

--- Week 8/Day 2/Animal_project/info/views.py
+++ Week 8/Day 2/Animal_project/info/views.py
@@ -19,10 +19,15 @@ def animal(request,id):
 
 def all_animals(request):
     all_animals=Animal.objects.all()
-    link = f'../animal/'
+    print(all_animals[0])   
+    for i in range (len(all_animals)):
+        all_animals[i].link =(str(f'../animal/{all_animals[i].id}'))
+
     context={
-        'content':all_animals
+        'content':all_animals,
+           
     }
+    print(all_animals[0].link)
     return render(request,'all_animals.html',context)
    
 
